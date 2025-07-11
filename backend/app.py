60de02167bffa24ede1500c84290b8fc3529acab
Dev Gandhi <gandhiDX02@gmail.com>
2025-07-11 01:12:50 -0400

    Allowed CORS from 5173 - Frontend

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,14 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins="http://localhost:5173", supports_credentials=True)
 
 @app.route('/api/parse-resume', methods = ['POST'])
 def parse_resume():
+    if request.method == 'OPTIONS':
+            # Preflight request
+            return '', 200
+
     data = request.get_json()
     name = data.get('name', 'Unknown')
     return jsonify({
