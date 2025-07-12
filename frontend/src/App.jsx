e2bf58aacfd271e4b77bd22fc994feb4642b8daa
Dev Gandhi <gandhiDX02@gmail.com>
2025-07-12 14:59:06 -0400

    Logic for FileUpload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,32 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  return True
+  const [file, setFile] = useState(null);
+  const [text, setText] = useState("");
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  }
+
+  const handleUpload = async () => {
+    // Alerts if there is no file uploaded
+    if (!file) return alert("Please upload a PDF file."); 
+
+    const formData = new FormData();
+    formData.append("resume", file);
+
+    try {
+      const response = axios.post('http://127.0.0.1:5000/api/parse-resume', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      setText(response.data.text);
+    } catch (err) {
+      console.error(err);
+      alert('Upload Failed.');
+    }
+  };
 }
 
 export default App
