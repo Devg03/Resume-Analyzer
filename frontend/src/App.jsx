00310fcb089583311cf4a0f417b3492faa07527e
Dev Gandhi <gandhiDX02@gmail.com>
2025-07-12 14:48:44 -0400

    Clean up since frontend and backend communicates as intended

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,34 +3,7 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [name, setName] = useState("");
-  const [response, setResponse] = useState(null);
-
-  const handleSubmit = async () => {
-    try {
-      const res = await axios.post("http://127.0.0.1:5000/api/parse-resume", {
-        name: name
-      });
-      setResponse(res.data.message);
-    } catch (err) {
-      console.error(err);
-      setResponse("Error occurred");
-    }
-  };
-
-  return (
-    <div>
-      <h1>AI Resume Analyzer</h1>
-      <input 
-        type="text"
-        placeholder='Enter name'
-        value={name}
-        onChange={(e) => setName(e.target.value)} 
-      />
-      <button onClick = {handleSubmit}>Submit</button>
-      {response && <p>{response}</p>}
-    </div>
-  );
+  return True
 }
 
 export default App
