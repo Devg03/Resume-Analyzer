7d2b64f04f7b46c42cebd19731fa3171cfa0fd49
Dev Gandhi <gandhiDX02@gmail.com>
2025-07-23 22:42:47 -0400

    Constraints ensurance for uploaded pdf

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,24 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import fitz
+from pdfminer.high_level import extract_text
+import tempfile
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 
 @app.route('/api/parse-resume', methods = ['POST'])
 def parse_resume():
-    text = "Succesfully Grabbed"
-    return jsonify(text)
+    if 'resume' not in request.files:
+        return jsonify({"error": "No File Uploaded"}), 400
+    
+    file = request.files["resume"]
+    if file.filename == '':
+        return jsonify({"error": "Empty filename"}), 400
+
+    if not file.filename.endswith(".pdf"):
+        return jsonify({"error": "File must be a PDF"}), 400
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
